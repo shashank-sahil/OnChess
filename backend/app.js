3b3fceeb4ef1f9b2bdcd86393104ba50a06875f7
Shashank Sahil <shashank.mdb2004@gmail.com>
2025-09-06 05:03:15 +0530

    Initial Setup , started routing

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express.js')
+const app = express()
+
+app.get('/' , (req,res)=>{
+
+// do stuff for the home page 
+
+})
+
+
+app.get('/login' , (req,res)=>{
+
+    // initiate login
+})
