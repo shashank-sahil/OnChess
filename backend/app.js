b41721514220ae8ddb44baaf73746d4df26fd7d9
Shashank Sahil <shashank.mdb2004@gmail.com>
2025-10-22 20:57:50 +0530

    frontend

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 import http from "http";
 import { reqType } from "./requestHandler.js";
-
 import { getTokens, getAuthClient, getAuthUrl ,parseJwt} from "./googleAuth.js";
-
+import cors from 'cors';
 
 
 
@@ -10,7 +9,21 @@ const HOST = 'localhost';
 const PORT = 3000;
 
 
+
 const server = http.createServer((req, res) => {
+  
+// Always set these headers for every request
+res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+}
+
     const request = new reqType(req);
     console.log(request.url.pathname)
 
